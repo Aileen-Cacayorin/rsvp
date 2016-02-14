1a2aab8b5cd2a05292c69f9861734e3980bf0bc6
Aileen Cacayorin <acacayorin@gmail.com>
2016-02-13 18:46:41 -0800

    refactor locations controller

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -25,23 +25,17 @@ class LocationsController < ApplicationController
     if @user == @event.user
       @location.save
 
-    #if location already exists, location is not saved but the guests room with corresponding location_id is saved
-    elsif @location.event_location_exists(@event)
-      @existing_location = @location.get_existing_location(@event)
-      @start_date = @event.start_date
-      @end_date = @event.end_date
-      @guest = @event.find_guest(@user).id
-      @room = Room.create(:start_date => @start_date, :end_date => @end_date, :location_id => @existing_location.id, :event_id => @event.id, :guest_id => @guest)
-      @room.save
-
-    #if location does not exist, saves location and adds guest room
     else
-      @location.save
-      @start_date = @event.start_date
-      @end_date = @event.end_date
       @guest = @event.find_guest(@user).id
-      @room = Room.create(:start_date => @start_date, :end_date => @end_date, :location_id => @location.id, :event_id => @event.id, :guest_id => @guest)
-      @room.save
+
+      if @location.event_location_exists(@event)   #if location exists, guest room with location_id is saved
+        @room_location = @location.get_existing_location(@event)
+
+      else #if location does not exist, saves location and adds guest room
+        @location.save
+        @room_location = @location
+      end
+      @room = Room.create(:start_date => @event.start_date, :end_date => @event.end_date, :location_id => @room_location.id, :event_id => @event.id, :guest_id => @guest)
     end
     redirect_to user_event_path(@user, @event)
   end
