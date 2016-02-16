f646d0e8a24e9ee99f1a42c759068bb64de017f7
Aileen Cacayorin <acacayorin@gmail.com>
2016-02-15 21:55:47 -0800

    redo google maps without gmaps gem. draws map but no markers yet

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,43 @@
 //= require underscore
 //= require gmaps/google
 //= require_tree .
+
+var map;
+
+function initMap(locations) {
+  var locations;
+  var event_location;
+  locations = locations.replace(/&quot;/g, '"');
+  new_locations = JSON.parse(locations);
+  main_location = new_locations[0]
+
+  var bounds;
+  bounds = new google.maps.LatLngBounds();
+  map = new google.maps.Map(document.getElementById('map-canvas'), {
+  center: {lat: main_location.latitude, lng: main_location.longitude},
+  zoom: 12
+  });
+
+  var markers = [];
+  new_locations.forEach(function(location) {
+    var address = [];
+    address.push(location.name),
+    address.push(location.latitude);
+    address.push(location.longitude);
+    markers.push(address);
+  });
+  console.log(markers)
+
+  for (var i = 0; i < markers.length; i++ ) {
+       var position = new google.maps.LatLng(markers[i][1], markers[i][2]);
+       bounds.extend(position);
+       marker = new google.maps.Marker({
+           position: position,
+           map: map,
+           title: markers[i][0]
+
+       });
+     }
+  map.fitBounds(bounds);
+
+}
