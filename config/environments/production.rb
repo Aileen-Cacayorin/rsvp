d165dd33d9b59eba5310f75be9455582abe7a1ff
Aileen <aileencacayorin@gmail.com>
2016-02-26 11:26:42 -0800

    precompile static assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,6 @@ Rails.application.configure do
   config.cache_classes = true
 
   # Code is not reloaded between requests.
-  config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
